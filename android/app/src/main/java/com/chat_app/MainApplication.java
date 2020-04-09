7f1a8fcce55b0904640b10023e292fd3f2852948
heiz99 <haziqazfar041@gmail.com>
2020-04-10 05:24:27 +0800

    Fixed typo in MainApplication.java - missing closing bracket

--- android/app/src/main/java/com/chat_app/MainApplication.java
+++ android/app/src/main/java/com/chat_app/MainApplication.java
@@ -42,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSBundleFile() {
             return CodePush.getJSBundleFile();
+        }
       };
 
   @Override
